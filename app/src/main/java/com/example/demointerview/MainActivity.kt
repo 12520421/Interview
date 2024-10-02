72d90c4055c35e236a1a831c06759619fed44bed
thongnguyen <ndthong.uit@gmail.com>
2024-10-02 14:48:10 +0700

    feat: solution for test 1

--- app/src/main/java/com/example/demointerview/MainActivity.kt
+++ app/src/main/java/com/example/demointerview/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.example.demointerview
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.demointerview.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         val navView: BottomNavigationView = binding.navView
 
@@ -31,5 +32,29 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        println("haha : ${solution(10, 5, 6, 4, 8)}")
+        println("haha : ${solution(10, 5, 6, 4, 9)}")
+        println("haha : ${solution(5, 3, 7, 4, 6)}")
     }
+
+    // solution for test 1
+    private fun solution(value1: Int, weight1: Int, value2: Int, weight2: Int, maxW: Int): Int {
+        val totalWeight = weight1 + weight2
+
+        if (totalWeight <= maxW)
+            return value1 + value2
+
+        if (weight1 <= maxW && weight2 <= maxW)
+            return if (value1 > value2) value1 else value2
+
+        if (weight1 <= maxW)
+            return value1
+
+        if (value2 <= maxW)
+            return value2
+
+        return 0
+    }
+
+
 }
